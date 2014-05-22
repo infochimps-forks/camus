326b3d28c6b1613ba379bbe4e00541d4a4be09b2
Josh Bronson <josh@infochimps.com>
2014-05-22 13:14:27 -0500

    adding sweeper for line-delimited json

--- camus-sweeper/src/main/java/com/linkedin/camus/sweeper/CamusSweeper.java
+++ camus-sweeper/src/main/java/com/linkedin/camus/sweeper/CamusSweeper.java
@@ -269,6 +269,13 @@ public class CamusSweeper extends Configured implements Tool
         {
           FileInputFormat.addInputPath(job, stat.getPath());
         }
+        else
+        {
+	  // NOTE: adding this case. It sure seems like it would have
+	  // been okay originally. Maybe there were originally
+	  // metadata files mixed in... --Josh
+          FileInputFormat.addInputPath(job, stat.getPath());
+        }
       }
     }
 
